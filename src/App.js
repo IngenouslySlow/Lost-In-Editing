acece47b6126e0b0ef9a8c423067279e13e55038
Being Cryptiic <krishnasaimarupudi114@gmail.com>
2021-05-17 12:28:01 +0530

    5. StylingAbout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 //Importing components
 import AboutUs from "./Pages/AboutUs";
 
+//Import global style
+import GlobalStyle from "./Components/GlobalStyle";
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
