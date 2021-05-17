63034ac8831b8b69a8e174a3b3ae8b0f78c78166
Being Cryptiic <krishnasaimarupudi114@gmail.com>
2021-05-17 09:43:12 +0530

    2. About Section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+//Importing components
+import AboutUs from "./Pages/AboutUs";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <AboutUs />
     </div>
   );
 }
---
+++ src/Pages/AboutUs.js
@@ -0,0 +1,13 @@
+import React from "react";
+//Importing the pages
+import AboutSection from "../Components/AboutSection";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <AboutSection />
+    </div>
+  );
+};
+
+export default AboutUs;
